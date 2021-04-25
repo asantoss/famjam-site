5d86c8daaa2d88589ebc40f43b63327d40a66e54
alexander <alexsantosantana@live.com>
2021-04-25 16:34:31 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Main/>
+</template>
+
+<script setup>
+import Main from './components/Main.vue'
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,254 @@
+<!--
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ]
+  }
+  ```
+-->
+<template>
+  <div class="bg-white">
+    <header>
+      <Popover class="relative bg-white">
+        <div
+          class="flex justify-between items-center max-w-7xl mx-auto px-4 py-6 sm:px-6 md:justify-start md:space-x-10 lg:px-8"
+        >
+          <div class="flex justify-center flex-1">
+            <a href=".">
+              <img class="h-32 w-32" src="../assets/logo.png" alt="" />
+            </a>
+          </div>
+        </div>
+      </Popover>
+    </header>
+
+    <main>
+      <!-- Hero section -->
+      <div class="relative">
+        <div class="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100" />
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div class="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
+            <div class="absolute inset-0">
+              <img
+                class="h-full w-full object-cover"
+                src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
+                alt="People working on laptops"
+              />
+              <div
+                class="absolute inset-0 main"
+                style="mix-blend-mode: multiply"
+              />
+            </div>
+            <div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
+              <h1
+                class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl"
+              >
+                <span class="block text-white">Fam Jam Podcast</span>
+              </h1>
+              <p
+                class="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-3xl"
+              >
+                The best place to learn about the media, cultural and societal
+                impact of different topics week to week.
+              </p>
+              <div
+                class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center"
+              >
+                <div
+                  class="space-y-4 align-center flex justify-center sm:gap-5"
+                >
+                  <a
+                    href="#player"
+                    class="appearance-none mx-auto flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-gray-800 bg-white hover:bg-jam sm:px-8"
+                  >
+                    Listen Now
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Team />
+      <div class="flex items-center justify-center bg-gray-100">
+        <Player id="player" class="w-96" />
+      </div>
+
+      <!-- Stats section -->
+
+      <!-- CTA Section -->
+    </main>
+
+    <footer class="bg-gray-50" aria-labelledby="footerHeading">
+      <h2 id="footerHeading" class="sr-only">Footer</h2>
+      <div class="max-w-7xl mx-auto pt-16 pb-8 px-4 sm:px-6 lg:pt-24 lg:px-8">
+        <div class="xl:grid xl:grid-cols-3 xl:gap-8">
+          <div class="grid grid-cols-2 gap-8 xl:col-span-2">
+            <div class="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3
+                  class="text-sm font-semibold text-gray-400 tracking-wider uppercase"
+                >
+                  Company
+                </h3>
+                <ul class="mt-4 space-y-4">
+                  <li v-for="item in footerNavigation.company" :key="item.name">
+                    <a
+                      :href="item.href"
+                      class="text-base text-gray-500 hover:text-gray-900"
+                    >
+                      {{ item.name }}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="mt-12 xl:mt-0">
+            <h3
+              class="text-sm font-semibold text-gray-400 tracking-wider uppercase"
+            >
+              Subscribe to get updates
+            </h3>
+            <p class="mt-4 text-base text-gray-500">
+              The latest episodes, sent to your inbox weekly.
+            </p>
+            <form class="mt-4 sm:flex sm:max-w-md">
+              <label for="emailAddress" class="sr-only">Email address</label>
+              <input
+                type="email"
+                name="emailAddress"
+                id="emailAddress"
+                autocomplete="email"
+                required=""
+                class="appearance-none min-w-0 w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-4 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:placeholder-gray-400"
+                placeholder="Enter your email"
+              />
+              <div class="mt-3 rounded-md sm:mt-0 sm:ml-3 sm:flex-shrink-0">
+                <button
+                  type="submit"
+                  class="w-full flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white main"
+                >
+                  Subscribe
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div
+          class="mt-12 border-t border-gray-200 pt-8 md:flex md:items-center md:justify-between lg:mt-16"
+        >
+          <div class="flex space-x-6 md:order-2">
+            <a
+              v-for="item in footerNavigation.social"
+              :key="item.name"
+              :href="item.href"
+              class="text-gray-400 hover:text-gray-500"
+            >
+              <span class="sr-only">{{ item.name }}</span>
+              <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
+            </a>
+          </div>
+          <p class="mt-8 text-base text-gray-400 md:mt-0 md:order-1">
+            &copy; 2020 Fam Jam Podcast. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+import { defineComponent, h } from "vue";
+import {
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+} from "@headlessui/vue";
+import {
+  InboxIcon,
+  MenuIcon,
+  SparklesIcon,
+  XIcon,
+} from "@heroicons/vue/outline";
+import { ChevronDownIcon } from "@heroicons/vue/solid";
+import Team from "./Team.vue";
+import Player from "./Player.vue";
+const footerNavigation = {
+  solutions: [
+    { name: "Marketing", href: "#" },
+    { name: "Analytics", href: "#" },
+    { name: "Commerce", href: "#" },
+    { name: "Insights", href: "#" },
+  ],
+  support: [
+    { name: "Pricing", href: "#" },
+    { name: "Documentation", href: "#" },
+    { name: "Guides", href: "#" },
+    { name: "API Status", href: "#" },
+  ],
+  company: [
+    { name: "About", href: "#" },
+    { name: "Blog", href: "#" },
+  ],
+  social: [
+    {
+      name: "GitHub",
+      href: "#",
+      icon: defineComponent({
+        render: () =>
+          h("svg", { fill: "currentColor", viewBox: "0 0 24 24" }, [
+            h("path", {
+              "fill-rule": "evenodd",
+              d:
+                "M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z",
+              "clip-rule": "evenodd",
+            }),
+          ]),
+      }),
+    },
+  ],
+};
+
+export default {
+  components: {
+    Popover,
+    PopoverButton,
+    PopoverGroup,
+    PopoverPanel,
+    ChevronDownIcon,
+    InboxIcon,
+    MenuIcon,
+    SparklesIcon,
+    XIcon,
+    Team,
+    Player,
+  },
+  setup() {
+    return {
+      footerNavigation,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.main {
+  background: rgb(2, 0, 36);
+  background: linear-gradient(
+    180deg,
+    hsl(243, 100%, 7%) 0%,
+    hsl(0, 0%, 38%) 30%,
+    hsl(66, 86%, 51%) 100%
+  );
+}
+</style>
